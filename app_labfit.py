e803a9fbe917539e7c200e4c2fea96e856bef077
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-17 10:45:03 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-import math as mt
-import os
-from matplotlib.transforms import IdentityTransform
+
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 
 st.set_page_config(layout="wide")
 st.title("App para el ajuste de datos experimentales")
@@ -12,12 +11,13 @@ st.markdown(" ### Ajuste lineal $ y = ax + b~~~~~~~~~~~~~~~~~~~~~~~~$  Ajuste po
 
 with st.sidebar:
     st.markdown(" ## Elige la opción de ajuste ")
-    optfit = st.radio("Ajuste tipo:",('Lineal $ y = ax + b$', 'Potencial'))
+    optfit = st.radio("Ajuste tipo:",('Lineal', 'Potencial'))
     st.markdown(" ## Elige el número de datos (>2)")
     nd = st.number_input('',min_value=2, max_value=20, value=2, step=1)
     st.markdown(" ## Etiquetas de los ejes")
     labelx = st.text_input('Eje x', 'X')
     labely = st.text_input('Eje y', 'Y')
+    datain = st.button('Ingresar datos datos')
 
     st.image('Dorado.jpg')
     st.markdown(""" 
@@ -32,3 +32,6 @@ with st.sidebar:
              
              """)
 
+
+if datain:
+    for i in range(1,nd):
