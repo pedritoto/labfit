471ff11a6b364035b8aedf950aefa09c023246ad
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-17 14:59:47 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -50,7 +50,9 @@ if nd != 0:
     #ydata = response['data']['Y'].to_numpy()
     xdata = df['X']
     ydata = df['Y']
-    st.write(ydata)
+    xdata
+    ydata
+    #st.write(ydata)
     fig = plt.figure(figsize=(4, 4), dpi=200)
     ax = fig.add_axes([0.03,0.055,0.95,0.95])
     ax.set_facecolor('azure')
