6ef5c00f721b9a56586384dacbd640c38ab429d4
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-17 16:10:04 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -52,12 +52,12 @@ if nd != 0:
     ydata=np.empty(nd, dtype=float) 
 
     for i in range (0,nd):
-        #st.write('---',float(response['data']['X'][i]),response['data']['Y'][i]) 
         xdata[i]=float(response['data']['X'][i])
         ydata[i]=float(response['data']['Y'][i])
-        #st.write(xdata[i],ydata[i])
-
-    st.write(ydata[1])
+    maxx = np.max(xdata)
+    xmin = np.min(xdata)
+    xx=np.linspace(minx, maxx, num=40, endpoint=True, dtype=float)
+    xx
     fig = plt.figure(figsize=(4, 4), dpi=200)
     ax = fig.add_axes([0.03,0.055,0.95,0.95])
     ax.set_facecolor('azure')
