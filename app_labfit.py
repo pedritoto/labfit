e39e9f69afc3b9c68973f9c48bac798042f3ea8e
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-18 09:05:58 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -76,16 +76,20 @@ if nd != 0:
             st.pyplot(fig)
             plt.savefig('plot.png')
             st.markdown("### Ajuste:")
+            nu1="{:.3f}".format(abs(coef[0]))
+            nu2="{:.3f}".format(abs(coef[1]))
             if coef[0]<0.0:
                 if coef[1]<0.0:
-                    stri = "### $ y = -$ " + " {:.3f}".format(abs(coef[0])) + " $x - $"+ " {:.3f}".format(abs(coef[1]))
+                    stri = "### $ y = -" + nu1+ "x - "+nu2+"$"
                 else:
-                    stri = "### $ y = -$ " + " {:.3f}".format(abs(coef[0])) + " $x + $"+ " {:.3f}".format(abs(coef[1]))
+                    stri = "### $ y = -" + nu1+ "x + "+nu2+"$"
             else:
                 if coef[1]<0.0:
-                    stri = "### $ y = $ " + " {:.3f}".format(abs(coef[0])) + " $x - $"+ " {:.3f}".format(abs(coef[1]))
+                    stri = "### $ y = " + nu1+ "x - "+nu2+"$"
+                   # stri = "### $ y = $ " + " {:.3f}".format(abs(coef[0])) + " $x - $"+ " {:.3f}".format(abs(coef[1]))
                 else:
-                    stri = "### $ y = $ " + " {:.3f}".format(abs(coef[0])) + " $x + $"+ " {:.3f}".format(abs(coef[1]))                
+                    stri = "### $ y = " + nu1+ "x + "+nu2+"$"
+                    #stri = "### $ y = $ " + " {:.3f}".format(abs(coef[0])) + " $x + $"+ " {:.3f}".format(abs(coef[1]))                
             st.markdown(stri)
         else:
             # Function to calculate the power-law with constants a and b
