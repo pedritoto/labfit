205b7dce28e84d715dba0e503c8c66cf99bdc18f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-18 10:27:13 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -88,7 +88,7 @@ if nd != 0:
                     stri = " $ y = " + nu1+ "~x + "+nu2+"$"                
             st.markdown('####'+stri)
             st.markdown("### Gráfica:")
-            ax.plot(xdata, ydata, 'ro',label='Experimento')
+            ax.plot(xdata, ydata, 'ro',marker='D',label='Experimento')
             ax.plot(xx, coef[0]*xx+coef[1], '-k',label=stri)
             ax.legend(loc='upper left', shadow=True, fontsize='small')
             plt.savefig('plot.png')
