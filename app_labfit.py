d0eb4d0fee9e10d4a454bddd48d3695978b9fd0b
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-18 09:22:07 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -73,7 +73,7 @@ if nd != 0:
             ax.plot(xx, coef[0]*xx+coef[1], '-k',label='Ajuste')
             ax.legend(loc='upper center', shadow=True, fontsize='x-small')
 
-            ax.grid('--')
+            ax.grid(True,lw=0.8,linestyle='--',zorder=0)
             plt.xlabel(labelx)
             plt.ylabel(labely)
             st.pyplot(fig)
