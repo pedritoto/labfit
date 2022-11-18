8bdd64f0d088e96020771a7968da9c7b611446d7
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-18 09:19:13 -0600

    .

--- app_labfit.py
+++ app_labfit.py
@@ -71,6 +71,8 @@ if nd != 0:
             poly1d_fn = np.poly1d(coef) 
             ax.plot(xdata, ydata, 'ro',label='Experimento')
             ax.plot(xx, coef[0]*xx+coef[1], '-k',label='Ajuste')
+            ax.legend(loc='upper center', shadow=True, fontsize='x-large')
+
             ax.grid()
             plt.xlabel(labelx)
             plt.ylabel(labely)
